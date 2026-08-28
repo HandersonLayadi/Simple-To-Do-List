af0fb0430691a54671e7c69c89e392fb16f40c5f
Handerson Layadi <handersonlayadi2@gmail.com>
2026-08-28 10:52:50 +0700

    Update bridging py to json

--- todolist.py
+++ todolist.py
@@ -1,54 +1,68 @@
-#Task data (JSON)
+# Task data (JSON)
 
 import json
-import os 
+import os
 
-if os.path.exists("tasks.json") : 
-    with open ("tasks.json", "r") as file : 
+file_path = os.path.join(os.path.dirname(__file__), "tasks.json")
+
+
+# Load tasks
+if os.path.exists(file_path):
+    with open(file_path, "r") as file:
         tasks = json.load(file)
-
-else : 
+else:
     tasks = []
 
-#Menu 
 
-while True :
-    print ("\n--- TO DO LIST ---")
-    print ("1. Add Task")
-    print ("2. View Task")
-    print ("3. Remove Task")
-    print ("4. Exit")
-    
-    choice = input ("Choose an option : ")
-    
-    #Add a task
-    if choice == "1" : 
-        task = input("Enter a task : ")
-        tasks.append (task)
+def save_tasks():
+    print("SAVING:", tasks)
+    print("SAVING TO:", file_path)
+
+    with open(file_path, "w") as file:
+        json.dump(tasks, file)
+
+
+# Menu
+
+while True:
+    print("\n--- TO DO LIST ---")
+    print("1. Add Task")
+    print("2. View Task")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Choose an option: ")
+
+    # Add a task
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        save_tasks()
         print("Task Has Been Added!")
-        
-    #View a task    
-    elif choice == "2" :
-        for index, task in enumerate(tasks) : 
-            print (index + 1, ".", task)
-            
-    #Remove a task 
-    elif choice =="3" : 
-        if not tasks : 
+
+    # View a task
+    elif choice == "2":
+        for index, task in enumerate(tasks):
+            print(index + 1, ".", task)
+
+    # Remove a task
+    elif choice == "3":
+        if not tasks:
             print("Your task list is empty")
-        else : 
-            for index, task in enumerate(tasks) : 
-                print (f"{index + 1}. {task}")   
-            
-            task_num = int(input("Enter a teask to be removed : "))
-            
-            if 1 <= task_num <= len(tasks) : 
+        else:
+            for index, task in enumerate(tasks):
+                print(f"{index + 1}. {task}")
+
+            task_num = int(input("Enter a task to be removed: "))
+
+            if 1 <= task_num <= len(tasks):
                 removed = tasks.pop(task_num - 1)
-                print ("Removed : ", removed)
-            else : 
-                print ("Invalid Task Number")
-                
-    #Exit 
-    elif choice =="4" : 
-        print('Goodbye Master')
+                save_tasks()
+                print("Removed:", removed)
+            else:
+                print("Invalid Task Number")
+
+    # Exit
+    elif choice == "4":
+        print("Goodbye Master")
         break
